108ccfd1ec67a91aeeb033eb9069d0df12f60693
serge <kachanov.serge@gmail.com>
2024-11-26 15:33:23 +0200

    added graphql and blog posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,17 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `rr-video-client`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: "My First Gatsby Site",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 };
